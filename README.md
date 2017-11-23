b23f4c794e558a37569713a024b3a262048ce188
Ties de Kock <ties@tiesdekock.nl>
2017-11-23 16:35:00 +0100

    Add a raw recording mode to the script
    todo: Make the logic re-usable instead of duplicating code

--- README.md
+++ README.md
@@ -2,7 +2,8 @@ capture_downstreams.py:
 -----------------------
 
 A script that reads a channel file (see `downstreams.conf`) and records from
-each channel in this file using `dvbv5-zap`. This recording is then filtered
-using tshark.
+each channel in this file using `dvbv5-zap`. Optionally, the resulting mpeg
+ts file is filtered using tshark, resulting in a pcap file with only specific
+packet types.
 
 Script is written for Python 3.6
--- capture_downstreams.py
+++ capture_downstreams.py
@@ -5,12 +5,11 @@ This script works for me but it is fragile - error checking is non-existent.
 
 Usage:
 ```
-sudo `which python` capture_downstreams.py \
+sudo `which python` capture_downstreams.py raw \
     downstreams.conf \
-    --prefix "Recording" \
+    --prefix "Recording_`date +%F_%R`" \
     --path /media/recordings \
-    -t 15 \
-    --skip_pid 8191
+    -t 45
 ```
 """
 import argparse
@@ -71,14 +70,40 @@ def pid_filter(pids: List[int]) -> str:
     return " || ".join([f"(mp2t.pid == 0x{p:02x})" for p in pids])
 
 
-def dvb_record_channels(channel_config: io.StringIO,
-                        config_file: str,
-                        verbose: bool = False,
-                        prefix: str = 'Capture',
-                        path: str = '.',
-                        duration: int = 15,
-                        skip_pids: List[int] = [],
-                        pids: List[int] = []):
+def dvb_record_raw_channels(channel_config: io.StringIO,
+                            config_file: str,
+                            verbose: bool = False,
+                            prefix: str = 'Capture',
+                            path: str = '.',
+                            duration: int = 45) -> None:
+    """
+    Capture raw mpeg ts
+    """
+    for channel in read_channels(channel_config):
+        freq_mhz = channel.frequency // 10**6
+        output_file_name = f"{path}/{prefix}_{channel.name}_{freq_mhz}.ts"
+
+        capture_cmd = ['/usr/bin/dvbv5-zap',
+                       '-c', config_file,
+                       '-P',
+                       '-t', str(duration),
+                       channel.name,
+                       '-o', output_file_name]
+        LOG.info(" ".join(capture_cmd))
+        subprocess.call(capture_cmd)
+
+
+def dvb_record_filtered_channels(channel_config: io.StringIO,
+                                 config_file: str,
+                                 verbose: bool = False,
+                                 prefix: str = 'Capture',
+                                 path: str = '.',
+                                 duration: int = 45,
+                                 skip_pids: List[int] = [],
+                                 pids: List[int] = []) -> None:
+    """
+    Capture data and filter it by pid, save as pcapng
+    """
     filter_str = ""
     if skip_pids and pids:
         print("--skip_pid and --pid were both used at the same time, this is "
@@ -89,10 +114,9 @@ def dvb_record_channels(channel_config: io.StringIO,
     else:
         filter_str = f"({pid_filter(pids)})"
 
-
     for channel in read_channels(channel_config):
         freq_mhz = channel.frequency // 10**6
-        output_file_name = f"{path}/{prefix}_{channel.name}_{freq_mhz}.ts"
+        output_file_name = f"{path}/{prefix}_{channel.name}_{freq_mhz}.pcapng"
 
         capture_cmd = ['/usr/bin/dvbv5-zap',
                        '-c', config_file,
@@ -119,21 +143,33 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Record part of the DVB-C stream for each mux in a channel'
                     ' file and filter it by pid')
-    parser.add_argument('--verbose', action='store_true', default=False,
-                        help='verbose output')
-    parser.add_argument('-t', '--duration', type=int, default=15,
-                        help='Capture duration')
-    parser.add_argument('channel_config', type=argparse.FileType('r'),
-                        help='channel file in dvbv5-zap format')
-    parser.add_argument('--pid', dest='pids', type=int, action='append',
-                        help='pid(s) to keep, default=all')
-    parser.add_argument('--skip_pid', dest='skip_pids', type=int,
-                        action='append', help='pid(s) to skip, '
-                        'pid and skip_pid are mutually exclusive')
-    parser.add_argument('--prefix', type=str, default='Capture',
-                        help='Prefix for the file names')
-    parser.add_argument('--path', type=str, default='.',
-                        help='Directory to save files in')
+
+    subs = parser.add_subparsers()
+
+    raw = subs.add_parser('raw', help='Save raw ts')
+    raw.set_defaults(func=dvb_record_raw_channels)
+
+    filtered = subs.add_parser('filtered', help='Save a filtered .pcap file')
+    filtered.set_defaults(func=dvb_record_filtered_channels)
+
+    filtered.add_argument('--pid', dest='pids', type=int, action='append',
+                          help='pid(s) to keep, default=all')
+    filtered.add_argument('--skip_pid', dest='skip_pids', type=int,
+                          action='append', help='pid(s) to skip, '
+                          'pid and skip_pid are mutually exclusive')
+    # Add the shared options to both sub-parsers
+    for p in [raw, filtered]:
+        p.add_argument('channel_config', type=argparse.FileType('r'),
+                       help='channel file in dvbv5-zap format')
+        p.add_argument('--verbose', action='store_true', default=False,
+                       help='verbose output')
+        p.add_argument('-t', '--duration', type=int, default=45,
+                       help='Capture duration, 45s is needed to ensure that '
+                       'two ranging replies are received for cable modems')
+        p.add_argument('--prefix', type=str, default='Capture',
+                       help='Prefix for the file names')
+        p.add_argument('--path', type=str, default='.',
+                       help='Directory to save files in')
 
     args = parser.parse_args()
 
@@ -143,8 +179,9 @@ if __name__ == '__main__':
     else:
         logging.getLogger().setLevel(logging.INFO)
 
-    if args:
-        dvb_record_channels(config_file=args.channel_config.name,
-                            **{k: v for k, v in args.__dict__.items() if v})
+    if args and hasattr(args, 'func'):
+        args.func(config_file=args.channel_config.name,
+                  **{k: v for k, v in args.__dict__.items()
+                     if v and k != 'func'})
     else:
         parser.print_help()
